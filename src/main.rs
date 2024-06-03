c3ee013b8798e53a3152cc1c774b1fa69a278000
zac460 <zac460@hotmail.co.uk>
2024-06-03 07:11:15 +0100

    Add arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(name = "roundup")]
+#[command(about = "Utility to count lines of code in a project")]
+struct Args {
+    dir: String,
+    filetype: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    CLI-based lines-of-code analyser. E.g.,
+        `roundup` for every type of file
+        `roundup rs` for Rust
+    */
+
+    let args = Args::parse();
+
+    println!("Directory: {}", args.dir);
+
+    if let Some(arg2) = args.filetype {
+        println!("Filetype: {}", arg2);
+    } else {
+        println!("Filetype: Not provided");
+    }
 }
